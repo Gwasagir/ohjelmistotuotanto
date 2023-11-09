66f380ab7e6c926b6c84fc50ee921df8dbd4a00b
Niko Keurulainen <keurulainen.niko@gmail.com>
2023-11-09 17:00:40 +0200

    almost done exc 2. Need to make it pretty

--- viikko2/project-reader/src/project_reader.py
+++ viikko2/project-reader/src/project_reader.py
@@ -1,6 +1,6 @@
 from urllib import request
 from project import Project
-
+import toml
 
 class ProjectReader:
     def __init__(self, url):
@@ -9,7 +9,20 @@ class ProjectReader:
     def get_project(self):
         # tiedoston merkkijonomuotoinen sisältö
         content = request.urlopen(self._url).read().decode("utf-8")
-        print(content)
+        # print(content)
+        parsed_content = toml.loads(content).get('tool').get('poetry')
+        # print(parsed_content)
+        
+        project_name = parsed_content.get('name')
+        project_description = parsed_content.get('description')
+
+        project_dependencies = []
+        for key in parsed_content.get('dependencies'):
+            project_dependencies.append(key)
+    
+        project_dev_dependencies = []
+        for key in parsed_content.get('group').get('dev').get('dependencies'):
+            project_dev_dependencies.append(key)
 
         # deserialisoi TOML-formaatissa oleva merkkijono ja muodosta Project-olio sen tietojen perusteella
-        return Project("Test name", "Test description", [], [])
+        return Project(project_name , project_description, project_dependencies, project_dev_dependencies)
