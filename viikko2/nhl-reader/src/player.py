921c880d6a897fc8660d947529a9be54a2204ac7
Niko Keurulainen <keurulainen.niko@gmail.com>
2023-11-16 20:34:47 +0200

    done exc 10

--- viikko2/nhl-reader/src/player.py
+++ viikko2/nhl-reader/src/player.py
@@ -1,6 +1,12 @@
 class Player:
     def __init__(self, dict):
         self.name = dict['name']
-    
+        self.team = dict ['team']
+        self.nationality = dict['nationality']
+        self.goals = dict['goals']
+        self.assists = dict['assists']
+        self.penalties = dict['penalties']
+        self.games = dict['games']
+
     def __str__(self):
-        return self.name
+        return f"{self.name} team: {self.team} goals: {self.goals} assists: {self.assists}"
